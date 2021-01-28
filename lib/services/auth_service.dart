7bbd6f71af9f09105924edb70126e805d91f03b3
Haezal Musa <ezalepy@gmail.com>
2021-01-29 01:15:14 +0800

    clean up code

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,9 @@
-import 'package:starterpack/models/models.dart';
 import '../services/api.dart';
 
 class AuthService {
   Api api = new Api();
 
-  Future<Map<String,dynamic>> login(Map<String, dynamic> body) async {
+  Future<Map<String, dynamic>> login(Map<String, dynamic> body) async {
     final response = await api.postAsync('auth/login', body);
     return response;
   }
